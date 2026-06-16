d5cc2a852c782ff1f0741097f726a6ce8cfed8b3
Kris Sankaran <sankaran.kris@gmail.com>
2026-06-16 11:44:49 +0200

    script to download multiomics data

---
+++ case_studies/tcga_brca/download_tcga.R
@@ -0,0 +1,122 @@
+#!/usr/bin/env Rscript
+#
+# This script downloads the TCGA data needed for the multi-omics disease
+# prediction case study.
+#
+#    Rscript download_tcga.R
+
+library(TCGAbiolinks)
+library(SummarizedExperiment)
+library(stringr)
+library(purrr)
+library(fs)
+library(glue)
+
+OUT_DIR <- path("data", "raw")
+dir_create(OUT_DIR, recursive = TRUE)
+
+# Truncate barcodes to patient level (TCGA-XX-XXXX)
+shorten <- \(x) substr(x, 1, 12)
+
+# Remove duplicate columns by name
+dedup_cols <- \(mat) mat[, !duplicated(colnames(mat)), drop = FALSE]
+
+# Save matrix as TSV and log dimensions
+save_tsv <- function(mat, fname) {
+    path <- OUT_DIR / fname
+    write.table(mat, path, sep = "\t", quote = FALSE, col.names = NA)
+    message(glue("  -> {fname}  ({nrow(mat)} features × {ncol(mat)} samples)"))
+    invisible(path)
+}
+
+
+# mRNA expression
+message("=== mRNA (STAR FPKM) ===")
+mrna_q <- GDCquery(
+    project = "TCGA-BRCA",
+    data.category = "Transcriptome Profiling",
+    data.type = "Gene Expression Quantification",
+    workflow.type = "STAR - Counts",
+    sample.type = "Primary Tumor"
+)
+GDCdownload(mrna_q, method = "api", files.per.chunk = 50)
+mrna_se <- GDCprepare(mrna_q)
+
+mrna_mat <- log1p(assay(mrna_se, "fpkm_unstrand"))
+rownames(mrna_mat) <- rowData(mrna_se)$gene_name
+mrna_mat <- mrna_mat[!duplicated(rownames(mrna_mat)), ]
+colnames(mrna_mat) <- shorten(colnames(mrna_mat))
+mrna_mat <- dedup_cols(mrna_mat)
+save_tsv(mrna_mat, "TCGA.BRCA.sampleMap_HiSeqV2.tsv")
+
+
+# miRNA expression
+message("=== miRNA ===")
+mirna_q <- GDCquery(
+    project = "TCGA-BRCA",
+    data.category = "Transcriptome Profiling",
+    data.type = "miRNA Expression Quantification",
+    sample.type = "Primary Tumor"
+)
+GDCdownload(mirna_q, method = "api", files.per.chunk = 50)
+mirna_se <- GDCprepare(mirna_q)
+
+mirna_mat <- log1p(assay(mirna_se, "reads_per_million_miRNA_mapped"))
+colnames(mirna_mat) <- shorten(colnames(mirna_mat))
+mirna_mat <- dedup_cols(mirna_mat)
+save_tsv(mirna_mat, "TCGA.BRCA.sampleMap_miRNA_HiSeq_gene.tsv")
+
+
+# Protein expression (RPPA)
+message("=== Protein RPPA ===")
+protein_q <- GDCquery(
+    project = "TCGA-BRCA",
+    data.category = "Proteome Profiling",
+    data.type = "Protein Expression Quantification",
+    sample.type = "Primary Tumor"
+)
+GDCdownload(protein_q, method = "api", files.per.chunk = 50)
+protein_data <- GDCprepare(protein_q)
+
+# Handle both SummarizedExperiment and data.frame return types
+if (is(protein_data, "SummarizedExperiment")) {
+    protein_mat <- assay(protein_data)
+} else {
+    # data.frame: first column = antibody names, remaining numeric columns = samples
+    num_cols <- map_lgl(protein_data, is.numeric) %>% which()
+    protein_mat <- as.matrix(protein_data[, num_cols])
+    rownames(protein_mat) <- protein_data[[1]]
+}
+colnames(protein_mat) <- shorten(colnames(protein_mat))
+protein_mat <- dedup_cols(protein_mat)
+save_tsv(protein_mat, "TCGA.BRCA.sampleMap_RPPA_RBN.tsv")
+
+
+# Clinical data
+message("=== Clinical ===")
+clin <- GDCquery_clinic("TCGA-BRCA", "clinical")
+
+# Map GDC primary_diagnosis to histological_type
+htype <- ifelse(
+    str_detect(clin$primary_diagnosis, regex("lobular", ignore_case = TRUE)) &
+        !str_detect(clin$primary_diagnosis, regex("in situ", ignore_case = TRUE)),
+    "Infiltrating Lobular Carcinoma",
+    ifelse(
+        str_detect(clin$primary_diagnosis, regex("duct", ignore_case = TRUE)) &
+            !str_detect(clin$primary_diagnosis, regex("in situ", ignore_case = TRUE)),
+        "Infiltrating Ductal Carcinoma",
+        NA_character_
+    )
+)
+
+clin_out <- data.frame(
+    row.names = clin$submitter_id,
+    histological_type = htype,
+    age_at_initial_pathologic_diagnosis = round(clin$age_at_diagnosis / 365.25),
+    gender = clin$gender,
+    race = clin$race,
+    stringsAsFactors = FALSE
+)
+save_tsv(clin_out, "TCGA.BRCA.sampleMap_BRCA_clinicalMatrix.tsv")
+
+message("\nAll done. Run pipeline.py to train models.")
